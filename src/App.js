f70d57989ffc24e8bf8cf19832f59493637907da
AICode97 <56427790+AICode97@users.noreply.github.com>
2020-11-11 11:30:42 +0100

    Login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import './App.css';
+import LoginForm from './login/LoginForm';
+import URLSettings from './settings'
+
 
 function App() {
   return (
     <div className="App">
       <h2>Suh dude</h2>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path={URLSettings.getURL("Login")}> <LoginForm /> </Route>
+        
+        </Switch>
+        
+      </Router>
     </div>
   );
 }
+const Header = () => {
+  return (
+    <ul className="header">
+     
+      <li><NavLink activeClassName="active" to={URLSettings.getURL("Login")}>Login</NavLink></li>
+
+    
+    </ul>
+  )
+}
+
 
 export default App;
